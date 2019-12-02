a0dbe88111ef2888fb6c10a2b80fbef4134bd184
Simon Kern <skjerns>
2019-12-02 14:48:24 +0100

    added channel renaming procedure

---
+++ preparation/channel_renaming.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 14:05:33 2019
+@author: Simon
+
+Use this file to rename channel to a common channel name
+
+"""
+
+import sys
+sys.path.append("..")
+from tqdm import tqdm
+import ospath
+import edf_utils
+
+remove = ['C3', 'C4', 'EOGl', 'EOGr', 'M1', 'M2', 'A1', 'A2', 'EOGV', 'Fz', 'Oz',
+          'Plethysmogram', 'Pleth', 'Akku', 'Numeric Aux10', 'O1', 'O2', 'FlowPAP', 
+          'Druck', 'li Arm', 'Skin potential', 'Numeric Aux9']
+
+ch_mapping = {
+           'C4:A1':     'EEG C4-A1',
+           'C3:A2':     'EEG C3-A2',
+           'F4:A1':     'EEG F4-A1',
+           'Fz:A2':     'EEGF z-A2',
+           'O2:A1':     'EEG O2-A1',
+           'Oz:A1':     'EEG Oz-A1',
+           'Oz:A2':     'EEG Oz-A2',
+           'Fz:A1':     'EEG Fz-A1',
+           'EOGl:A1':   'EOGl-A1',
+           'EOGl:A2':   'EOG LOC-A2',
+           'EOGr:A1':   'EOG ROC-A1',
+           'EOGr:A2':   'EOG ROC-A2',
+           'EOG1:A2':   'EOG LOC-A2',
+           'EOG2:A1':   'EOG ROC-A1',
+           'EOG1':      'EOG LOC-A2',
+           'EOG2':      'EOG ROC-A1',
+           'EOG LOC':   'EOG LOC-A2',
+           'EOG ROC':   'EOG ROC-A1',
+           'ECG':       'ECG I',
+           'EMGchin':      'EMG Chin',
+           'EMG EMGTible': 'EMG Chin',
+           'EMG EMGTibri': 'EMG Chin',
+           'EMG EMGarmle': 'EMG Aux1',
+           'EMG EMGarmri': 'EMG Aux2',
+           'EMG':  'EMG Chin',
+           'EMG1': 'EMG Chin',
+           'EMG2': 'EMG Aux1',
+           'EMG EMGchin':'EMG Chin',
+           'EKG': 'ECG I',
+           'PLM l':'PLM l',
+           'PLM r':'PLM r',
+           'Bein l': 'PLM l',
+           'Bein r': 'PLM r',
+           'Snore':'Snore',
+           'Abdomen':'Abdomen',
+           'Thorax':'Thorax',
+           'Snoring':'Snore',
+           'Schnarc':'Snore',
+           'RIP.Thrx':'Effort THO',
+           'RIP.Abdom':'Effort ABD',
+           'Abdom':'Abdomen',
+           'SPO2':'SpO2',
+           'SpO2':'SpO2',
+           'Puls':'PulseRate',
+           'Pulse':'PulseRate',
+           'Airflow':'Flow Patient',
+           'Flow':'Flow Patient',
+           'Lage':'Body',
+           'Beweg.':'Accelerometer',
+           'Summe Effort':'Effort',
+           'Licht':'Lux Ambient',
+           'Licht':'Lux Ambient',
+           'Druck Flow':'Druck Flow',
+           'Microphone':'Microphone',
+           }
+
+
+
+
+datafolder = 'z:/NT1-HRV'
+output = 'z:/renamed'
+files = ospath.list_files(datafolder, exts='edf', subfolders=True)
+
+# We rename the channels to have the same names in all recordings
+
+names = []
+new_names = []
+for i, file in enumerate(tqdm(files)):
+    header = edf_utils.read_edf_header(file)
+    channels = header['channels']
+    new_file = ospath.join(output, ospath.basename(file))
+    if ospath.exists(new_file): continue
+
+    signal_headers = []
+    signals = []
+    for ch_nr in tqdm(range(len(channels))):
+        signal, signal_header, _ = edf_utils.read_edf(file, digital=True, 
+                                                    ch_nrs=ch_nr, verbose=False)
+        ch = signal_header[0]['label']
+        ch = ch.replace(':M', ':A')
+        if ch in remove: continue
+        if ch in ch_mapping or ch in ch_mapping.values():
+            print('{} to {}'.format(ch, ch_mapping[ch]))
+            ch = ch_mapping[ch]
+            signal_header[0]['label']=ch
+        else:
+            print('no mapping for {}'.format(ch))
+        signal_headers.append(signal_header[0])
+        signals.append(signal.squeeze())
+
+    edf_utils.write_edf(new_file, signals, signal_headers, header,digital=True)
--- preparation/pseudonymisierung.py
+++ preparation/pseudonymisierung.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 13 16:24:17 2019
-
 @author: SimonKern
+
+This script removes all patient related information from an edf
 """
-names = []
-new_names = []
+import sys
+sys.path.append("..")
 
 from tqdm import tqdm
 import ospath
-import tools
+import edf_utils
 
-datafolder = 'C:/Users/SimonKern/Desktop/nc1/nc1/'
-files = ospath.list_files(datafolder)
+datafolder = 'C:/Users/SimonKern/Desktop/NT1-HRV/NT1/'
+files = ospath.list_files(datafolder, exts='edf')
 
+# We rename the channels to have the same names in all recordings
+
+names = []
+new_names = []
 for i, file in enumerate(files):
     i = i+1
-    header = tools.read_edf_header(file)
+    header = edf_utils.read_edf_header(file)
     name = header['patientname']
     n_chs = len(header['channels'])
     new_name = 'NC1{:0>2}'.format(i)
@@ -30,11 +35,9 @@ for i, file in enumerate(files):
     signal_headers = []
     signals = []
     for ch_nr in tqdm(range(n_chs)):
-        signal, signal_header, _ = tools.read_pyedf(file, digital=True, 
+        signal, signal_header, _ = edf_utils.read_edf(file, digital=True, 
                                                     ch_nrs=ch_nr, verbose=False)
         signal_headers.append(signal_header[0])
         signals.append(signal.squeeze())
-    
 
-
-    tools.write_pyedf(new_file, signals, signal_headers, header,digital=True)
+    edf_utils.write_edf(new_file, signals, signal_headers, header,digital=True)
