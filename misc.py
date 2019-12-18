db34860dbd72ded8f8d805c1a1abdf647fe8a6d6
Simon Kern <skjerns>
2019-12-18 13:17:01 +0100

    added file chooser

---
+++ misc.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 12:46:37 2019
+
+@author: Simon
+"""
+import os
+from tkinter import  Tk
+from tkinter.filedialog import askopenfilename
+
+
+def choose_file(default_dir=None,exts='txt', title='Choose file'):
+    """
+    Open a file chooser dialoge with tkinter.
+    
+    :param default_dir: Where to open the dir, if set to None, will start at wdir
+    :param exts: A string or list of strings with extensions etc: 'txt' or ['txt','csv']
+    :returns: the chosen file
+    """
+    root = Tk()
+    root.iconify()
+    if isinstance(exts, str): exts = [exts]
+    name = askopenfilename(initialdir=None,
+                           parent=root,
+                           title = title,
+                           filetypes =(*[("File", "*.{}".format(ext)) for ext in exts],
+                                       ("All Files","*.*")))
+    root.update()
+    root.destroy()
+    if not os.path.exists(name):
+        print("No file exists")
+    else:
+        return name
+
