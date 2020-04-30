2d08bdb4a983621e0a7171f727f43a40d1983022
skjerns <14980558+skjerns@users.noreply.github.com>
2020-04-30 13:06:43 +0200

    rewrote anonymize script, optimize

--- preparation/1_anonymize.py
+++ preparation/1_anonymize.py
@@ -17,6 +17,7 @@ import pandas as pd
 import misc
 from misc import codify
 from joblib import delayed, Parallel
+import tempfile
 
 #######################
 # Settings for datasets
@@ -32,128 +33,131 @@ documents = cfg.documents # contains the path to the nt1-hrv-documents folder in
 #######################
 ch_mapping = cfg.mapping_channels
 
-#%%#############
-## Actual code
 ###############
-
-
-
-def anonymize_and_streamline(dataset_folder, target_folder, threads=False):
+#%%anonymize###
+###############
+def anonymize_and_streamline(old_file, target_folder):
     """
     This function loads the edfs of a folder and
     1. removes their birthdate and patient name
     2. renames the channels to standardized channel names
-    3. saves the files in another folder with a non-identifyable code
+    3. saves the files in another folder with a non-identifyable 
     4. verifies that the new files have the same content as the old
     """
+    # load the two csvs with the edfs that we dont process and where the ECG is upside down
     to_discard = [line[0] for line in misc.read_csv(cfg.edfs_discard) if line[2]=='1']
     to_invert = [line[0] for line in misc.read_csv(cfg.edfs_invert)]
 
     # Here we read the list of controls and patients with their age and gender
-    # NOTE:
     mappings = misc.read_csv(cfg.controls)
     mappings.extend(misc.read_csv(cfg.patients))
     mappings = dict([[name, {'gender':gender, 'age':age}] for name, gender, age in mappings])
 
-    files = ospath.list_files(dataset_folder, exts='edf', subfolders=True)
-    old_names = []
-    new_names = []
-    for i, old_file in enumerate(tqdm(files, leave=True)):
-        old_name = ospath.splitext(ospath.basename(old_file))[0]
-        new_name = codify(old_name)
-        # we use a temporary file to write and then rename it
-        tmp_name = ospath.join(ospath.dirname(old_file), 'tmp')
-        if new_name in new_names:
-            other_old = old_name[new_names.index(new_name)]
-            raise Exception('Hash collision, file is already in database. '\
-                            '{},{}->{}'.format(other_old, old_name, new_name))
-        elif old_name in to_discard:
-            print('EDF is marked as corrupt and will be discarded')
-            continue
-        
-        new_file = ospath.join(target_folder, new_name + '.edf')
-        old_names.append(old_name)
-        new_names.append(new_name)  
+    # old name is the personalized file without file extension, e.g. thomas_smith(1)
+    old_name = ospath.splitext(ospath.basename(old_file))[0]
+    # new name is the codified version without extension e.g '123_45678'
+    new_name = codify(old_name)
+    
+    # use a temporary file to write and then move it, 
+    # this avoids half-written files that cannot be read later
+    tmp_name = tempfile.TemporaryFile().name
 
-        if ospath.exists(new_file): 
-            print ('New file extists already {}'.format(new_file))
+    if old_name in to_discard:
+        print('EDF is marked as corrupt and will be discarded')
+        return
+    
+    # this is where the anonymized file will be stored
+    new_file = ospath.join(target_folder, new_name + '.edf')
 
-        else:
+
+    if ospath.exists(new_file): 
+        print ('New file extists already {}'.format(new_file))
+
+    else:
         # anonymize
-            print ('Writing {} from {}'.format(new_file, old_name))
-            assert ospath.isfile(old_file), f'{old_file} does not exist'
-            signals, signal_headers, header = sleep_utils.read_edf(old_file, 
-                                                                   digital=True,
-                                                                   verbose=False)
-            header['birthdate'] = ''
-            header['patientname'] = new_name
-            header['patientcode'] = new_name
-            header['gender'] = mappings[old_name]['gender']
-            header['age'] = mappings[old_name]['age']
+        print ('Writing {} from {}'.format(new_file, old_name))
+        assert ospath.isfile(old_file), f'{old_file} does not exist'
+        signals, signal_headers, header = sleep_utils.read_edf(old_file, 
+                                                               digital=True,
+                                                               verbose=False)
+        # remove patient info
+        header['birthdate'] = ''
+        header['patientname'] = new_name
+        header['patientcode'] = new_name
+        header['gender'] = mappings[old_name]['gender']
+        header['age'] = mappings[old_name]['age']
 
-            for shead in signal_headers:
-                ch = shead['label']
-                if ch in ch_mapping:
-                    ch = ch_mapping[ch]
-                    shead['label'] = ch
-                    
-
-            if old_name in to_invert:
-                for i,sig in enumerate(signals):
-                    label = signal_headers[i]['label'].lower()
-                    if label == cfg.ecg_channel.lower():
-                        signals[i] = -sig
-            
-            # we write to tmp to prevent that corrupted files are not detected later
-            print ('Writing tmp for {}'.format(new_file))
-            sleep_utils.write_edf(tmp_name, signals, signal_headers, header, 
-                                  digital=True, correct=True)
-            
-            print ('Verifying tmp for {}'.format(new_file))
-            if not old_name=='B0036': # embarrasing hack, as dmin/dmax dont in these files
-                sleep_utils.compare_edf(old_file, tmp_name, verbose=False)
-            
-            # now we rename the tmp file.
-            shutil.move(tmp_name, new_file)
-
-        # also copy additional file information ie hypnograms and kubios files
-        old_dir = ospath.dirname(old_file)
-        old_name = old_name.replace('_m', '').replace('_w', '') # remove gender from weitere nt1 patients
-        add_files = ospath.list_files(old_dir, patterns=[f'{old_name}*txt', f'{old_name}*dat', f'{old_name}*mat'])
-        for add_file in add_files: 
-            new_add_file = ospath.join(target_folder, 
-                                       ospath.basename(add_file.replace(old_name, new_name)))
-            if ospath.exists(new_add_file):continue
-            try:
-                new_add_file = new_add_file.replace('-Schlafprofil', '')
-                new_add_file = new_add_file.replace('_sl','')
-                new_add_file = new_add_file.replace('.txt', '.hypno').replace('.dat', '.hypno')
-                shutil.copy(add_file, new_add_file)
-            except Exception as e:
-                print(e)
-    return old_names, new_names
+        # rename channels to a unified notation, e.g. EKG becomes ECG I
+        for shead in signal_headers:
+            ch = shead['label']
+            if ch in ch_mapping:
+                ch = ch_mapping[ch]
+                shead['label'] = ch
+                
+        # Invert the ECG channel if necessary
+        if old_name in to_invert:
+            for i,sig in enumerate(signals):
+                label = signal_headers[i]['label'].lower()
+                if label == cfg.ecg_channel.lower():
+                    signals[i] = -sig
         
+        # we write to tmp to prevent that corrupted files are not left
+        print ('Writing tmp for {}'.format(new_file))
+        sleep_utils.write_edf(tmp_name, signals, signal_headers, header, 
+                              digital=True, correct=True)
+        
+        # verify that contents for both files match exactly
+        print ('Verifying tmp for {}'.format(new_file))
+        # embarrasing hack, as dmin/dmax dont in this files after inverting
+        if not old_name=='B0036': 
+            sleep_utils.compare_edf(old_file, tmp_name, verbose=False)
+        
+        # now we move the tmp file to its new location.
+        shutil.move(tmp_name, new_file)
+
+    # also copy additional file information ie hypnograms and kubios files
+    old_dir = ospath.dirname(old_file)
+    old_name = old_name.replace('_m', '').replace('_w', '') # remove gender from weitere nt1 patients
+    add_files = ospath.list_files(old_dir, patterns=[f'{old_name}*txt', f'{old_name}*dat', f'{old_name}*mat'])
+    for add_file in add_files: 
+        # e.g. .mat or .npy etc etc
+        new_add_file = ospath.join(target_folder, 
+                                   ospath.basename(add_file.replace(old_name, new_name)))
+        if ospath.exists(new_add_file):continue
+        # hypnograms will be copied to .hypno
+        try:
+            new_add_file = new_add_file.replace('-Schlafprofil', '')
+            new_add_file = new_add_file.replace('_sl','')
+            new_add_file = new_add_file.replace('.txt', '.hypno').replace('.dat', '.hypno')
+            shutil.copy(add_file, new_add_file)
+        except Exception as e:
+            print(e)
+    return old_name, new_name
+
+
+
+
+#%% Main
 if __name__ == '__main__':
     print('running in parallel. if you don\'t see output, start with python.exe')
     
-    results = Parallel(n_jobs=4, backend='loky')\
-    (delayed(anonymize_and_streamline)(dataset, target_folder=target_folder) for dataset in datasets.values())
-    i=0
-    for old_names, new_names in results:
-        csv_file = ospath.join(documents, 'mapping_{}.csv'.format(list(datasets.keys())[i]))
-        csv = pd.DataFrame(zip(old_names, new_names))
-        csv.to_csv(csv_file, header=None, index=False, sep=';')
-        i+=1
-        
-    # save as well in one large file
-    all_old_names = []
-    all_new_names = []
-    for old_names, new_names in results:
-        all_old_names.extend(old_names)
-        all_new_names.extend(new_names)
+    # first get all edfs in all dataset folders
+    files = [];  # cheeky workaround for not functioning list comprehension .extend
+    _ = [files.extend(ospath.list_files(folder, exts='edf', subfolders=True)) for folder in datasets.values()]
+    
+    
+    results = Parallel(n_jobs=4, backend='loky')(delayed(
+              anonymize_and_streamline)(file, target_folder=target_folder) for file in tqdm(files, desc='processing edfs'))
+    
+    # remove discarded files
+    results = [res for res in results if not res is None]
+    
+    # check for hash collision
+    assert len(set(list(zip(*results))[1]))==len(list(zip(*results))[1]),\
+         'ERROR: Hash collision! Check thoroughly.'
+    
     csv_file = ospath.join(documents, 'mapping_all.csv')
-    csv = pd.DataFrame(zip(all_old_names, all_new_names))
-    csv.to_csv(csv_file, header=None, index=False, sep=';')
+    pd.DataFrame(results).to_csv(csv_file, header=None, index=False, sep=';')
 
         
         
